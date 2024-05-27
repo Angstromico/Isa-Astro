f4da45c809391267cfbb51053b48806b0b994290
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2024-05-27 12:59:20 -0400

    Scroll animation on BMS Section

---
+++ src/components/BMSSection/Container.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from 'react'
+type Props = {
+  children: JSX.Element | JSX.Element[]
+}
+const Container = ({ children }: Props) => {
+  const sectionRef = useRef<HTMLElement>(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible')
+          } else {
+            entry.target.classList.remove('visible')
+          }
+        })
+      },
+      { threshold: 0.1 }
+    )
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current)
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current)
+      }
+    }
+  }, [])
+
+  return (
+    <section ref={sectionRef} className='bms'>
+      {children}
+    </section>
+  )
+}
+export default Container
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 import Header from './Header.astro'
 import Footer from './Footer/index.astro'
 import MainBanner from './MainBanner.astro'
-import BMSSection from './BMSSection.astro'
+import BMSSection from './BMSSection/index.astro'
 
 export { Header, Footer, MainBanner, BMSSection }
